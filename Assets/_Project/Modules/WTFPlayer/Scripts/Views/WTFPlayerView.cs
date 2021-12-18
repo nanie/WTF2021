a1f9221ee01123e7fa794bf2da1c4dbee6cb493e
Camilla Camatta <camillacamatta@gmail.com>
2021-12-18 14:40:05 -0300

    Module Created

---
+++ Assets/_Project/Modules/WTFPlayer/Scripts/Views/WTFPlayerView.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace UnityMVC.Events
+{
+    public partial class WTFPlayerControllerEvents
+    {
+        public Action<View.View> onViewEnabled;
+        public Action<View.View> onViewDisabled;
+        public Action<View.View> onViewDestroyed;
+    }
+}
+
+namespace UnityMVC.Model
+{
+    public class WTFPlayerViewModel : MVCModel
+    {
+    }
+}
+
+namespace WTF.Player
+{
+    public partial class WTFPlayerView
+    {
+        // MVC properties available: Controller, Events and Data
+
+        protected override void SolveDependencies()
+        {
+        }
+        
+        protected override void RegisterControllerEvents()
+        {
+        }
+
+        protected override void UnregisterControllerEvents()
+        {
+        }
+
+        protected override void MVCAwake()
+        {
+        }
+
+        protected override void MVCStart()
+        {
+        }
+
+        protected override void MVCLateStart()
+        {
+        }
+
+        protected override void MVCUpdate()
+        {
+        }
+    }
+}
