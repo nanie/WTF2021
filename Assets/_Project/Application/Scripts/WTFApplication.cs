6a90296698db6a1132c6529927996061eb396c2f
Camilla Camatta <camillacamatta@gmail.com>
2021-12-18 14:32:28 -0300

    Project Created

---
+++ Assets/_Project/Application/Scripts/WTFApplication.cs
@@ -0,0 +1,8 @@
+namespace WTF
+{
+    // This is your Application static class intended to contain factories and static data. Be careful.
+    public static class WTFApplication
+    {
+        
+    }
+}
